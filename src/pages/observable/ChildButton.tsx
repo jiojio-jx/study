dc6209093aef9d597a8678492ca6b7b30c406b09
jiojio-jx <18306638509@183.com>
2022-08-26 11:12:41 +0800

    feat: add reative demo-多计数器使用

---
+++ src/pages/observable/ChildButton.tsx
@@ -0,0 +1,38 @@
+import { observable } from '@formily/reactive';
+import { observer } from '@formily/reactive-react';
+import React from 'react';
+import { useState } from 'react';
+import CounterStore, { CounterEnum } from './Store';
+
+type Props = {
+  name: string;
+  mode: CounterEnum;
+};
+
+export default observer((props: Props) => {
+  let [mode, setMode] = useState<{ value: CounterEnum }>(() => {
+    return observable({
+      value: props.mode,
+    });
+  });
+  let counter = CounterStore.get(mode.value);
+  console.log('Child Render');
+  return (
+    <div>
+      <h2>{props.name}</h2>
+      <div>{'当前mode为：' + mode.value + ',当前值为:' + counter}</div>
+      <button onClick={CounterStore.inc.bind(CounterStore, mode.value)}>加1</button>
+      <button onClick={CounterStore.dec.bind(CounterStore, mode.value)}>减1</button>
+      <button
+        onClick={() => {
+          if (mode.value == 'fish') {
+            mode.value = 'cat';
+          } else {
+            mode.value = 'fish';
+          }
+        }}>
+        {'切换mode'}
+      </button>
+    </div>
+  );
+});
--- src/pages/observable/index.tsx
+++ src/pages/observable/index.tsx
@@ -1,36 +1,78 @@
+import { observer } from '@formily/react';
 import { observable, autorun } from '@formily/reactive';
 import { Button } from 'antd';
 import React, { useEffect } from 'react';
+import ChildButton from './ChildButton';
 
-/** 学习、理解 @formily/reactive
- * 如何集中管理表单数据
- * - 集中管理数据
- * - 管理表单路径、多表单联动
+/**
+ * 全局数据状态管理 mobx、redux、redux-logger、Pinia（vue3）
+ * - state、action 方法管理
+ * - dispatch 执行 action， 更新数据 与 UI
+ *
+ * 那么在表单场景下
+ * - 管理多表单数据：多表单联动、删除｜修改｜增加｜更新
+ * - 管理表单事件触发、需要批量事件通知其他表单模块数据、进行数据更新
+ *
+ * 即多表单的数据抽象管理、就是一个发布-订阅模式
+ * - 在get阶段进行依赖收集
+ * - 在set阶段发布时间、批量更新
+ *
+ * - 倾听 set 操作
+ *  - observable
+ *  - observable.ref
+ *  - observable.box
+ *  - 预发糖
+ *    - action
+ *    - define
+ *    - model
+ *
+ * - 收集 get 操作、自动触发
+ *  - autorun
+ *  - computed
+ *  - reaction
+ *  - tracker
+ *  - observe
+ *
+ * - 批量触发
+ *  - batch
+ *  - batch.scope
  */
 
-// 创建 监听对象
-const obs = observable({
-  aa: 11,
-  bb: 22,
+let data = observable({
+  open: true,
+  buttons: [0],
 });
 
-// 监听 数据变化
-autorun(() => {
-  console.log(obs.aa);
+export default observer(() => {
+  console.log('Parent Render');
+  return (
+    <div>
+      <div>
+        <button
+          key="add"
+          onClick={() => {
+            data.buttons.push(data.buttons.length + 1);
+          }}>
+          添加一个
+        </button>
+        <button
+          key="clear"
+          onClick={() => {
+            data.buttons = [];
+          }}>
+          清除
+        </button>
+        <button
+          key="other"
+          onClick={() => {
+            data.open = !data.open;
+          }}>
+          状态：{data.open ? '打开' : '关闭'}
+        </button>
+      </div>
+      {data.buttons.map(id => {
+        return <ChildButton key={id} name={'按钮' + id} mode={'fish'} />;
+      })}
+    </div>
+  );
 });
-
-const Index = () => {
-  useEffect(() => {
-    // 计算属性缓存
-    const computed = observable.computed(() => obs.aa + obs.bb);
-    console.log('===>', computed.value);
-  }, []);
-
-  const dispose = () => {
-    obs.aa++;
-  };
-
-  return <Button onClick={dispose}>打印</Button>;
-};
-
-export default Index;
